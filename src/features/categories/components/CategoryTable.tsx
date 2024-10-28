056970c1a02d80b682015ef1f16514e196271272
Williams Barriquero <willbrr.dev@gmail.com>
2024-10-28 07:32:17 -0300

    feat: :sparkles: refatorando categories table funções

--- src/features/categories/components/CategoryTable.tsx
+++ src/features/categories/components/CategoryTable.tsx
@@ -1,4 +1,13 @@
-import { GridFilterModel } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, IconButton, Typography } from "@mui/material";
+import {
+  DataGrid,
+  GridColDef,
+  GridFilterModel,
+  GridRenderCellParams,
+  GridRowsProp,
+} from "@mui/x-data-grid";
+import { Link } from "react-router-dom";
 import { Results } from "../../../types/Category";
 
 type Props = {
@@ -22,4 +31,87 @@ export function CategoryTable({
   handleFilterChange,
   handleOnPageSizeChange,
   handleDelete,
-}: Props) {}
+}: Props) {
+  const slotProps = {
+    toolbar: {
+      showQuickFilter: true,
+      quickFilterProps: { debounceMs: 500 },
+    },
+  };
+
+  const rows: GridRowsProp = data ? mapDataToGridRows(data) : [];
+
+  const columns: GridColDef[] = [
+    {
+      field: "name",
+      flex: 1,
+      headerName: "Name",
+      renderCell: renderNameCell,
+    },
+    {
+      field: "isActive",
+      flex: 1,
+      headerName: "Active",
+      renderCell: renderIsActiveCell,
+      type: "boolean",
+    },
+    { field: "createdAt", headerName: "Created At", flex: 1 },
+    {
+      field: "id",
+      flex: 1,
+      headerName: "Actions",
+      renderCell: renderActionsCell,
+      type: "string",
+    },
+  ];
+
+  function mapDataToGridRows(data: Results) {
+    const { data: categories } = data;
+    return categories.map((category) => ({
+      id: category.id,
+      name: category.name,
+      description: category.description,
+      isActive: category.is_active,
+      createdAt: new Date(category.created_at).toLocaleDateString("pt-BR"),
+    }));
+  }
+
+  function renderNameCell(rowData: GridRenderCellParams) {
+    return (
+      <Link
+        style={{
+          textDecoration: "none",
+        }}
+        to={`/categories/edit/${rowData.id}`}
+      >
+        <Typography color="primary">{rowData.value}</Typography>
+      </Link>
+    );
+  }
+
+  function renderActionsCell(params: GridRenderCellParams) {
+    return (
+      <IconButton
+        aria-label="delete"
+        color="secondary"
+        onClick={() => handleDelete(params.value)}
+      >
+        <DeleteIcon />
+      </IconButton>
+    );
+  }
+
+  function renderIsActiveCell(rowData: GridRenderCellParams) {
+    return (
+      <Typography color={rowData.value ? "primary" : "secondary"}>
+        {rowData.value ? "Active" : "Inactive"}
+      </Typography>
+    );
+  }
+
+  return (
+    <Box sx={{ display: "flex", height: 600 }}>
+      <DataGrid rows={rows} columns={columns} />
+    </Box>
+  );
+}
