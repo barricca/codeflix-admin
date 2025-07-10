804126fad779bc761f066541fca947c9d35c8806
Williams Barriquero <williamsgomes45@gmail.com>
2025-07-09 22:46:38 -0300

    refactor: :art: ajusta busca de cast members

---
+++ src/features/cast-members/CreateCastMembers.tsx
@@ -0,0 +1,3 @@
+export default function CreateCastMembers() {
+	return <div>CreateCastMembers</div>;
+}
---
+++ src/features/cast-members/components/CastMemberForm.tsx
@@ -0,0 +1,89 @@
+import {
+	Box,
+	Button,
+	FormControl,
+	FormControlLabel,
+	FormGroup,
+	FormLabel,
+	Grid,
+	Radio,
+	RadioGroup,
+	Switch,
+	TextField,
+} from "@mui/material";
+
+import { Link } from "react-router-dom";
+import type { CastMember } from "../../../types/CastMembers";
+
+type Props = {
+	castMember: CastMember;
+	isDisabled?: boolean;
+	isLoading?: boolean;
+	handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+	handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	handleToggle: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export function CastMemberForm({
+	castMember,
+	isDisabled = false,
+	isLoading = false,
+	handleSubmit,
+	handleChange,
+}: Readonly<Props>) {
+	return (
+		<Box p={2}>
+			<form onSubmit={handleSubmit}>
+				<Grid container spacing={3}>
+					<Grid item xs={12}>
+						<FormControl fullWidth>
+							<TextField
+								required
+								name="name"
+								label="Name"
+								value={castMember.name || ""}
+								disabled={isDisabled}
+								onChange={handleChange}
+								inputProps={{ "data-testid": "name" }}
+							/>
+						</FormControl>
+					</Grid>
+					<Grid item xs={12}>
+						<FormGroup>
+							<FormLabel>Type</FormLabel>
+							<RadioGroup
+								aria-labelledby="type of cast member"
+								defaultValue="Director"
+								name="type"
+								onChange={handleChange}
+								value={castMember.type}
+							>
+								<FormControlLabel
+									value={1}
+									control={<Radio />}
+									label="Director"
+								/>
+								<FormControlLabel value={2} control={<Radio />} label="Actor" />
+							</RadioGroup>
+						</FormGroup>
+					</Grid>
+					<Grid item xs={12}>
+						<Box display={"flex"} gap={2}>
+							<Button variant="contained" component={Link} to="/cast-members">
+								Back
+							</Button>
+							<Button
+								type="submit"
+								variant="contained"
+								color="secondary"
+								disabled={isDisabled || isLoading}
+							>
+								{isLoading ? "Loading..." : "Save"}
+							</Button>
+						</Box>
+					</Grid>
+				</Grid>
+			</form>
+		</Box>
+	);
+}
