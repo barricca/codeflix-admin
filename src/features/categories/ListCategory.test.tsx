21ab39d63493cfe95d655429c244cf8031b406c6
Williams Barriquero <williamsgomes45@gmail.com>
2025-07-14 13:11:34 -0300

    test: :white_check_mark: interceptando request com msw

--- src/features/categories/ListCategory.test.tsx
+++ src/features/categories/ListCategory.test.tsx
@@ -1,9 +1,31 @@
-import { renderWithProviders } from "../../utils/test-utils";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { renderWithProviders, screen } from "../../utils/test-utils";
+import { baseUrl } from "../api/apiSlice";
 import { CategoryList } from "./ListCategory";
+import { categoryResponse } from "./mocks";
 
-describe("ListCategory", () => {
+export const handlers = [
+	rest.get(`${baseUrl}/categories`, (_, res, ctx) => {
+		return res(ctx.json(categoryResponse), ctx.delay(150));
+	}),
+];
+
+const server = setupServer(...handlers);
+
+describe("CategoryList", () => {
+	afterAll(() => server.close());
+	beforeAll(() => server.listen());
+	afterEach(() => server.resetHandlers());
+
 	it("should render correctly", () => {
 		const { asFragment } = renderWithProviders(<CategoryList />);
 		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render with loading state", () => {
+		renderWithProviders(<CategoryList />);
+		const loading = screen.getByRole("progressbar");
+		expect(loading).toBeInTheDocument();
 	});
 });
